5127b4e23910a4cc80bd7ffbe6e39d35efa4853d
DanielRodado <d4nielrodado@gmail.com>
2024-09-02 13:59:10 -0500

    Otras configuraciones para springdoc

--- src/main/java/com/example/taskservice/configurations/OpenApiConfig.java
+++ src/main/java/com/example/taskservice/configurations/OpenApiConfig.java
@@ -5,6 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "TaskService APP", version = "V1"))
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TaskService APP",
+                version = "V1",
+                description = "API for managing tasks in the TaskService application."
+        )
+)
 public class OpenApiConfig {
 }

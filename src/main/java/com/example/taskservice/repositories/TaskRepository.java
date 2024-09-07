cd13f026898e73ad5607a8a91c6d6d91b0e874bd
DanielRodado <d4nielrodado@gmail.com>
2024-09-07 10:41:35 -0500

    Método para buscar las taras de un usuario por su username

--- src/main/java/com/example/taskservice/repositories/TaskRepository.java
+++ src/main/java/com/example/taskservice/repositories/TaskRepository.java
@@ -2,6 +2,10 @@ package com.example.taskservice.repositories;
 
 import com.example.taskservice.models.TaskEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface TaskRepository extends ReactiveCrudRepository<TaskEntity, Long> {
+
+    Flux<TaskEntity> findByUserUsername(String userUsername);
+
 }
